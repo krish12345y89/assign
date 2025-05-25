295a608593446028112c78acf64242c72c6e018f
krish12345y89 <krisharora3406@gmail.com>
2025-05-25 18:26:36 +0530

    add

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ connectDB();
 
 const app = express();
 app.use(express.json());
+app.use("/",res.send("Pixisphere Backend Auth Running"));
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
