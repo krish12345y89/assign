12175c61759d1d8a20a06999f43e730c644b3cf7
krish12345y89 <krisharora3406@gmail.com>
2025-05-25 19:11:43 +0530

    ini

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
